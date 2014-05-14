4b221243e77cd671b16d817303d5a029578098b8
lukaszkaczmarekk <lukaszkaczmarekk@gmail.com>
2014-05-14 18:15:56 +0200

    Poprawka funkcji

--- kalkulator.py
+++ kalkulator.py
@@ -3,3 +3,18 @@ def nasza_funkcja(argument1, argument2);
        return argument
        
 print nasza_funkcja("Początek", "Koniec")
+
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+
+second_name = None
+
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
